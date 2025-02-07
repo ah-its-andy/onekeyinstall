51f8ef240664ac5030e4b49ddb76b3795b793252
Andrew <mykge@outlook.com>
2025-02-07 11:51:52 +0800

    Update install-aws-cli.sh

--- install-aws-cli.sh
+++ install-aws-cli.sh
@@ -2,8 +2,8 @@
 sudo echo 'Installing AWS CLI'
 sudo apt update
 sudo apt install -y unzip
-mkdir -p ~/install-aws-cli
+mkdir -p /tmp/install-aws-cli
 sudo echo "Download AWS CLI Package"
-wget -O ~/install-aws-cli/awscliv2.zip "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+wget -O /tmp/install-aws-cli/awscliv2.zip "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
 OLDPWD=$(pwd)
-cd ~/install-aws-cli && unzip awscliv2.zip && sudo ./aws/install && cd "$OLDPWD"
+cd /tmp/install-aws-cli && unzip awscliv2.zip && sudo ./aws/install && cd "$OLDPWD"
