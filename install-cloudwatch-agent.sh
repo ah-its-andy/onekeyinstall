2d2c7f0c76deee99fcc23363d7f41f19b1458dc3
andrew <mykge@outlook.com>
2025-02-13 12:11:43 +0800

    update

--- install-cloudwatch-agent.sh
+++ install-cloudwatch-agent.sh
@@ -3,10 +3,28 @@
 CLOUDWATCH_AGENT_CONFIG_PATH="/opt/aws/amazon-cloudwatch-agent/bin/config.json"
 CLOUDWATCH_AGENT_COMMON_CONFIG_PATH="/opt/aws/amazon-cloudwatch-agent/etc/common-config.toml"
 
+# Detect the OS
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+	echo "Detected OS: $OS"
+else
+    OS=$(uname -s)
+fi
+
 sudo echo 'Installing Dependencies of Docker CE'
 mkdir -p /tmp/install-cloudwatch-agent
-wget -O /tmp/install-cloudwatch-agent/amazon-cloudwatch-agent.deb https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-sudo dpkg -i -E /tmp/install-cloudwatch-agent/amazon-cloudwatch-agent.deb
+
+if [ "$OS" = "ubuntu" ]; then
+    wget -O /tmp/install-cloudwatch-agent/amazon-cloudwatch-agent.deb https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+    sudo dpkg -i -E /tmp/install-cloudwatch-agent/amazon-cloudwatch-agent.deb
+elif [ "$OS" = "amzn" ]; then
+    wget -O /tmp/install-cloudwatch-agent/amazon-cloudwatch-agent.rpm https://amazoncloudwatch-agent.s3.amazonaws.com/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+    sudo rpm -U /tmp/install-cloudwatch-agent/amazon-cloudwatch-agent.rpm
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
 
 sudo tee "$CLOUDWATCH_AGENT_COMMON_CONFIG_PATH" > /dev/null <<EOF
 [credentials]
--- install-docker-ce.sh
+++ install-docker-ce.sh
@@ -1,18 +1,42 @@
 #!/bin/sh
-sudo echo 'Installing Dependencies of Docker CE'
 
-sudo apt-get update
-sudo apt-get install ca-certificates curl -y
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Detect the operating system
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+    echo "Detected OS: $OS"
+else
+    echo "Unsupported OS"
+    exit 1
+fi
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+if [ "$OS" = "ubuntu" ]; then
+    sudo echo 'Installing Dependencies of Docker CE on Ubuntu'
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl -y
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo 'Installing Docker CE'
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y	
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    echo 'Installing Docker CE on Ubuntu'
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+elif [ "$OS" = "amzn" ]; then
+     sudo echo 'Installing Dependencies of Docker CE on Amazon Linux'
+      sudo yum update -y
+      sudo yum install ca-certificates curl docker -y
+
+      echo 'Starting Docker service on Amazon Linux'
+      sudo service docker start
+      sudo systemctl enable docker
+else
+    echo "Unsupported OS"
+    exit 1
+fi
